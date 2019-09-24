50e35e0d02d688a2f16b0a60851a1a472d135896
Benjamin <bscholtz.bds@gmail.com>
2019-09-24 12:59:26 +0200

    Set ZFS ARC max

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -6,7 +6,12 @@
 
   boot.kernelParams = [
     "zswap.enabled=1"
+    "zfs.zfs_arc_max=5000000000"
   ];
 
+  boot.extraModprobeConfig = ''
+    options zfs zfs_arc_max=5000000000
+  '';
+  
   boot.kernel.sysctl = { "vm.swappiness" = 10; };
 }
